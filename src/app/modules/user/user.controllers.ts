f11e64d1dd4f58eef9fc86aefd3ee2e7eccebbb2
Tasnimul <tasnimulsharan8962@gmail.com>
2025-01-15 16:36:27 +0600

    refector(user): refector user controller with catchAsync and sendResponse function

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,11 +1,18 @@
-import { Request, Response } from "express";
+import  httpStatus  from 'http-status';
 import { UserServices } from "./user.services";
+import catchAsync from "../../utils/catchAsync.";
+import sendResponse from "../../utils/sendResponse";
 
-const createUserController = (req: Request, res: Response) => {
+const createUserController = catchAsync(async(req, res) => {
     const result = UserServices.createUserService(req.body);
 
- res.status(200).json(result);
-}
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'User created successfully',
+        data: result
+    })
+})
 
 export const UserController = {
     createUserController

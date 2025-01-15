b5fd288aac73d5ead66f14e6d3ac8f8688a0a0dc
Tasnimul <tasnimulsharan8962@gmail.com>
2025-01-15 17:05:44 +0600

    refector(user): refector user route and user validation with zod validation schema

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { UserController } from "./user.controllers";
+import validateRequest from "../../../middlewares/validateRequest";
+import {
+  userUpdateValidationSchema,
+  userValidationSchema,
+} from "./user.validation";
 
-const route = Router()
+const route = Router();
 
-route.post('/create', UserController.createUserController)
-route.get('/get', UserController.getUserController)
-route.get("/:email", UserController.getUserByEmailController)
-route.put("/:id", UserController.updateUserController)
-route.delete("/:id", UserController.deleteUserController)
+route.post(
+  "/create",
+  validateRequest(userValidationSchema),
+  UserController.createUserController,
+);
+route.get("/get", UserController.getUserController);
+route.get("/:email", UserController.getUserByEmailController);
+route.put(
+  "/:id",
+  validateRequest(userUpdateValidationSchema),
+  UserController.updateUserController,
+);
+route.delete("/:id", UserController.deleteUserController);
 
-export const userRoute = route
+export const userRoute = route;
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,12 +1,23 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const userValidationSchema = z.object({
-  _id: z.number().optional(), // _id is optional
-  name: z.string().min(1, 'Name is required'), // Name is a required string
-  email: z.string().email('Invalid email format'), // Email should be a valid email string
-  password: z.string().min(8, 'Password must be at least 8 characters'), // Password should be a string with min length
-  role: z.enum(['admin', 'user']).optional(), // Role can be either 'admin' or 'user', optional
-  createdAt: z.date().optional(), // createdAt is optional and should be a Date
-  updatedAt: z.date().optional(), // updatedAt is optional and should be a Date
-  address: z.string().min(1, 'Address is required'), // Address is required
+  body: z.object({
+    _id: z.number().optional(), // _id is optional
+    name: z.string().min(1, "Name is required"), // Name is a required string
+    email: z.string().email("Invalid email format"), // Email should be a valid email string
+    password: z.string().min(8, "Password must be at least 8 characters"), // Password should be a string with min length
+    role: z.enum(["admin", "user"]).optional(), // Role can be either 'admin' or 'user', optional
+    address: z.string().min(1, "Address is required"), // Address is required
+  }),
+});
+
+export const userUpdateValidationSchema = z.object({
+  body: z.object({
+    _id: z.number().optional(), // _id is optional
+    name: z.string().optional(),
+    email: z.string().email("Invalid email format").optional(), // Email should be a valid email string
+    password: z.string().min(8, "Password must be at least 8 characters").optional(), // Password should be a string with min length
+    role: z.enum(["admin", "user"]).optional(), // Role can be either 'admin' or 'user', optional
+    address: z.string().min(1, "Address is required").optional(),
+  }),
 });

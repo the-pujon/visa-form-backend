6fe187110811b00917708dc531fd65bc9b0d1303
Tasnimul <tasnimulsharan8962@gmail.com>
2025-01-15 16:28:03 +0600

    feat(middleware): added middleware for authentication, error handling, handling not found and for zod validation parser

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,66 @@
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../utils/catchAsync.";
+import AppError from "../errors/AppError";
+import httpStatus from "http-status";
+import jwt, {
+  JsonWebTokenError,
+  JwtPayload,
+  TokenExpiredError,
+} from "jsonwebtoken";
+import config from "../config";
+import { UserModel } from "../modules/auth/auth.model";
+import { getCachedData } from "../utils/redis.utils";
+
+export const auth = (...requiredRoles: ("admin" | "user")[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You are not authorized. Login first",
+      );
+    }
+
+    try {
+      const decoded = jwt.verify(token, config.jwt_access_secret as string);
+
+      const { email, role } = decoded as JwtPayload;
+
+      const cachedToken = await getCachedData(`sparkle-car-service:user:${email}:token`);
+
+      if (cachedToken !== token) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "Token is not valid");
+      }
+
+      const user = await UserModel.isUserExist(email);
+
+      if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, "This user is not found!");
+      }
+
+      if (requiredRoles && !requiredRoles.includes(role)) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          "You have no access to this route",
+        );
+      }
+
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          "Your session has expired. Please login again.",
+        );
+      } else if (error instanceof JsonWebTokenError) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          "Invalid token. Please login again.",
+        );
+      }
+      throw error;
+    }
+  });
+};
---
+++ src/middlewares/notFoundRouteHandler.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+
+const notFoundRouteHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  return res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    statusCode: httpStatus.NOT_FOUND,
+    message: "Not Found",
+  });
+};
+
+export default notFoundRouteHandler;

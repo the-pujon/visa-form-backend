bac332461d98f8e98fc59d0d7b61725077fd77f3
Pujon Das Auvi <auvipujondas@gmail.com>
2025-01-16 14:35:02 +0600

    fix(lint): fixed linting issue

--- src/app/utils/cloudinaryUpload.ts
+++ src/app/utils/cloudinaryUpload.ts
@@ -1,32 +1,34 @@
-import { v2 as cloudinary } from 'cloudinary'
-import fs from 'fs'
-import configs from '../configs';
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import configs from "../configs";
 
-cloudinary.config({ 
-    cloud_name: configs.cloud_name, 
-    api_key: configs.cloud_api_key, 
-    api_secret: configs.cloud_api_secret
-  });
+cloudinary.config({
+  cloud_name: configs.cloud_name,
+  api_key: configs.cloud_api_key,
+  api_secret: configs.cloud_api_secret,
+});
 
-
-  export const cloudinaryUpload = (imageName: string, path: string)  => {
-console.log("uploading image")
-    return new Promise((resolve, reject) => {
-      cloudinary.uploader.upload(path, { public_id: imageName }, (err, result) => {
+export const cloudinaryUpload = (imageName: string, path: string) => {
+  // console.log("uploading image")
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      path,
+      { public_id: imageName },
+      (err, result) => {
         if (err) {
-            console.log("error uploading image", err)
-          reject(err)
+          // console.error("error uploading image", err)
+          reject(err);
         } else {
-          resolve(result)
-          console.log("Upload successful")
-        //   console.log(result)
+          resolve(result);
+          // console.log("Upload successful")
+          //   console.log(result)
 
-          fs.unlink(path,(err) => {
+          fs.unlink(path, (err) => {
             if (err) throw err;
-            console.log('File deleted!');
-          })
-          
+            // console.log('File deleted!');
+          });
         }
-      })
-    })
-  }
+      },
+    );
+  });
+};

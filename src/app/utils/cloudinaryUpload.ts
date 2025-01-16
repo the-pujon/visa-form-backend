612a1d371a62a0efb5d9ca3da568ebad246277cd
Pujon Das Auvi <auvipujondas@gmail.com>
2025-01-16 12:15:50 +0600

    feat(image): cloudinary upload functionality added with multer

--- src/app/utils/cloudinaryUpload.ts
+++ src/app/utils/cloudinaryUpload.ts
@@ -1,7 +1,28 @@
 import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
 
 cloudinary.config({ 
     cloud_name: 'my_cloud_name', 
     api_key: 'my_key', 
     api_secret: 'my_secret'
   });
+
+
+  export const cloudinaryUpload = (imageName: string, path: string)  => {
+
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(path, { public_id: imageName }, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+
+          fs.unlink(path,(err) => {
+            if (err) throw err;
+            console.log('File deleted!');
+          })
+          
+        }
+      })
+    })
+  }

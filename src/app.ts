99ee396b569014f58f88ee6f0b34f17d0f7aecc8
Pujon Das Auvi <auvipujondas@gmail.com>
2025-01-20 11:53:59 +0600

    created a linting error

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import router from './app/routes';
 // import notFoundRouteHandler from './app/middlewares/notFoundRouteHandler';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
-const app: Application = express();
+let app: Application = express();
 
 app.use(express.json());
 app.use(

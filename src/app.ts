1a88781d1aa0c18ce29cda6780f3248b9ac050b0
Pujon Das Auvi <auvipujondas@gmail.com>
2025-01-20 11:55:31 +0600

    fix(es Lint): fixed linting error

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import router from './app/routes';
 // import notFoundRouteHandler from './app/middlewares/notFoundRouteHandler';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
-let app: Application = express();
+const app: Application = express();
 
 app.use(express.json());
 app.use(
